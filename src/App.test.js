41584fd2b9e5a47fbc2a9a63267d9494c4b64cef
andrius <andriuspl@gmail.com>
2020-04-04 16:30:17 +0300

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/I was change on a feature branch/i);
   expect(linkElement).toBeInTheDocument();
 });
